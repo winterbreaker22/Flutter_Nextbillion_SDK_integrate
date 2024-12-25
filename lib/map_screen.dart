5bd123d6ef94a0413d269556a9a1a1d01839b4bf
winterbreaker22 <joejamesdev@hotmail.com>
2024-12-25 01:20:17 -0800

    Update map_screen.dart

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -19,7 +19,7 @@ class MapScreenState extends State<MapScreen> {
   @override
   void initState() {
     super.initState();
-    NextBillion.initNextBillion('YOUR_KEY');
+    NextBillion.initNextBillion('1ee3375a754f4137969a2b708d071124');
     NBNavigation.setUserId('miguel-first-nb');
     NBNavigation.getNBId().then((value) {
       logger.i('nb_id: $value');
@@ -34,7 +34,7 @@ class MapScreenState extends State<MapScreen> {
       appBar: AppBar(title: const Text('Route Map')),
       body: NBMap(
         onMapCreated: onMapCreated,
-        initialCameraPosition: CameraPosition(target: LatLng(widget.destination['latitude']!, widget.destination['longitude']!), zoom: 14),
+        initialCameraPosition: CameraPosition(target: LatLng(widget.destination['latitude']!, widget.destination['longitude']!), zoom: 6),
       ),
     );
   } 
@@ -49,12 +49,17 @@ class MapScreenState extends State<MapScreen> {
       RouteRequestParams requestParams = RouteRequestParams(
         origin: LatLng(widget.origin['latitude']!, widget.origin['longitude']!),
         destination: LatLng(widget.destination['latitude']!, widget.destination['longitude']!),
+        language: 'en',
         mode: ValidModes.truck,
       );
 
       final result = await NBNavigation.fetchRoute(requestParams);
       final navNextBillionMap = await NavNextBillionMap.create(controller);
       await navNextBillionMap.drawRoute(result.directionsRoutes);
+
+      List<DirectionsRoute> routes = result.directionsRoutes;
+      NavigationLauncherConfig config = NavigationLauncherConfig(route: routes.first, routes: routes, shouldSimulateRoute: true, themeMode: NavigationThemeMode.dark);
+      NBNavigation.startNavigation(config);
     } catch (e) {
       logger.e(e);
     }
